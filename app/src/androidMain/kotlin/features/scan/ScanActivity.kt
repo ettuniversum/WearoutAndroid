1f0c6d59faf34c082fe3767e4eb4349fe9427e9b
lorinbales <lorinbales@gmail.com>
2026-06-06 18:31:54 -0500

    Screen not overlapping banner

--- app/src/androidMain/kotlin/features/scan/ScanActivity.kt
+++ app/src/androidMain/kotlin/features/scan/ScanActivity.kt
@@ -14,10 +14,13 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
@@ -80,7 +83,11 @@ class ScanActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Column(Modifier.background(color = MaterialTheme.colors.background)) {
+                Column(
+                    Modifier
+                        .background(color = MaterialTheme.colors.background)
+                        .windowInsetsPadding(WindowInsets.safeDrawing)
+                ) {
                     val bluetooth = Bluetooth.availability.collectAsState(initial = null).value
                     AppBar(viewModel, bluetooth)
                     Box(Modifier.weight(1f)) {
--- app/src/androidMain/kotlin/features/sensor/SensorActivity.kt
+++ app/src/androidMain/kotlin/features/sensor/SensorActivity.kt
@@ -8,9 +8,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Slider
@@ -63,6 +67,7 @@ class SensorActivity : ComponentActivity() {
                 Column(
                     Modifier
                         .background(color = MaterialTheme.colors.background)
+                        .windowInsetsPadding(WindowInsets.safeDrawing)
                         .fillMaxSize()) {
                     TopAppBar(title = { Text("Wearout") })
 
